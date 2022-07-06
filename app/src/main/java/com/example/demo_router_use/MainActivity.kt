32458efa96fd66ffdc9e32e702debe716e8b73c8
“chenqun” <1715666965@qq.com>
2022-07-06 15:37:31 +0800

    使用发不好的注解处理插件

--- app/src/main/java/com/example/demo_router_use/MainActivity.kt
+++ app/src/main/java/com/example/demo_router_use/MainActivity.kt
@@ -2,7 +2,12 @@ package com.example.demo_router_use
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.chenchen.router.annotations.Destination
 
+@Destination(
+    url = "router://test-page",
+    destination = "测试页面"
+)
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
